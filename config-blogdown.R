9568b98e42ffb316c5d4583e9ebbe9bae6ac2922
Darragh Murray <darragh.murray@gmail.com>
2022-01-10 23:17:22 +1000

    Updates and deletions

--- config-blogdown.R
+++ config-blogdown.R
@@ -1,6 +1,6 @@
 blogdown::new_site(theme = "CaiJimmy/hugo-theme-stack")
 file.edit(".gitignore")
-blogdown::new_post(title = "A Reminder on the Importance of Exploratory Data Analysis: My First B2VB Entry", 
+blogdown::new_post(title = "The importance of exploratory data analysis: Exploring the first B2VB challenge", 
                    ext = '.Rmarkdown', 
                    subdir = "post")
 blogdown::serve_site()
